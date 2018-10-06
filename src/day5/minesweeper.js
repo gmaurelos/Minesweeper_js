2e138b5d5af37262cf22da768834ad76d84e19c7
Geo Mavr <gmaurelos@gmail.com>
2018-10-07 00:51:53 +0300

    Add Code cfor day5 project

---
+++ src/day5/minesweeper.js
@@ -0,0 +1,7 @@
+// creating empty board
+const blankLine = '  |   |  ';
+console.log('This is what an empty board would look like:'+'\n'+ blankLine+'\n'+blankLine+'\n'+blankLine);
+// creating a "guess" board
+const guessLine = '1 |   |  ';
+const bombline = '  | B |  ';
+console.log('This is what a board with a guess and a bomb on it would look like:'+'\n'+ guessLine+'\n'+bombline+'\n'+blankLine);
